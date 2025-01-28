0685bd8b346fe16971cc19352c28ff0d25ed3299
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2025-01-27 23:13:26 -0800

    Navbar menus added

--- app/src/NavBar.jsx
+++ app/src/NavBar.jsx
@@ -1,8 +1,17 @@
 function NavBar() {
     return (
         <div>
-            <header className="bg-white p-4">
+            <header className="bg-white p-4 flex justify-between">
+                {/*Left logo*/}
                 <h1 className="text-xl font-bold text-gray-800">StudyGram</h1>
+
+                {/*Right menus*/}
+                <div>
+                    <a className="p-4" href="">Home
+                    </a>
+                    <a className="p-4" href="">Profile
+                    </a>
+                </div>
             </header>
         </div>
     );
