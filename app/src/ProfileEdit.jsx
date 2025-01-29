bd517e58b3876d261235cc65c4168e92ce0a3149
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2025-01-28 20:14:22 -0800

    Static profile page + edit form added

--- app/src/ProfileEdit.jsx
+++ app/src/ProfileEdit.jsx
@@ -62,7 +62,6 @@ const ProfileEdit = () => {
                             className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit
                     </button>
                 </form>
-
             </section>
         </div>
     );
