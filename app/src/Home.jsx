c9640af91e82927c9932b400500b19202686d1ca
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2025-02-02 09:32:35 -0800

    search bar added to navbar

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -5,13 +5,7 @@ function Home() {
             <h1 className="text-4xl pt-40 font-semibold text-gray-800">
                 This is the home page for CS 35L project.
             </h1>
-            <div className="mt-10">
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="w-1/3 p-3 border border-gray-300 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
-                />
-            </div>
+
         </div>
     );
 }
