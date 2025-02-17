cbb816e3507ebb00e2cc60e098153312f1ce7f4b
kgoodman09 <86638214+kgoodman09@users.noreply.github.com>
2025-02-16 20:29:36 -0800

    Integrating the MongoDB database

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const mongoose = require('mongoose');
+const { User, Post, Comment, Tag, PostTag } = require('./user');
+
+mongoose.connect('mongodb://localhost:27017/studygram', { //insert where the database is located
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
 function App() {
   const [count, setCount] = useState(0)
 
