ad3df2b4587e1049c14db0e0a566ee9c30a9c083
Felicia Chen <feliciachen18@gmail.com>
2025-02-21 01:12:38 -0800

    connected backend and gitignore the .env file

--- app/backend/config/db.js
+++ app/backend/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '../../.env' }); // Load database and port
+dotenv.config({ path: ".env" }); // Load database and port
 
 mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB!');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
 });
 
 export default db;
