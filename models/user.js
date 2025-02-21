eee7aecf8f27c39727b65f504ecfc414241d0757
Felicia Chen <feliciachen18@gmail.com>
2025-02-21 01:12:38 -0800

    connected backend and gitignore the .env file

--- models/user.js
+++ models/user.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose');
-    
+const mongoose = require("mongoose");
+
 // Define the schema for the 'User' collection
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: { type: String, required: true, unique: true, default: null },
+    email: { type: String, required: true, unique: true, default: null },
     name: { type: String, default: null },
     posts: { type: [String], default: [] }, //array of the posts created by this user
-    bio: { type: String, default: null},
+    bio: { type: String, default: null },
     profilePic: { type: String, default: null }, //URL of the pfp
-    password: { type: String, default: null } // hashing function (?)
-}, { collection: 'user' });
+    password: { type: String, default: null }, // hashing function (?)
+  },
+  { collection: "user" }
+);
 
 module.exports = userSchema;
