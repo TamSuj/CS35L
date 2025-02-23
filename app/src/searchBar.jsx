87df267225352837559b794bd4534185d3627dc4
Megan Luu <megluu04@gmail.com>
2025-02-23 00:37:11 -0800

    change search bar to form insead of input

--- app/src/searchBar.jsx
+++ app/src/searchBar.jsx
@@ -1,12 +1,24 @@
-function SearchBar() {
-    return (
-        <input
-            type="text"
-            placeholder="Search..."
-            // className="w-1/3 p-2 pl-5 border border-gray-300 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
+import React, { useState } from 'react';
 
-            className="p-2 pl-5 border border-gray-300 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all w-full"
-        />
+function SearchBar() {
+    const [query, setQuery] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Searched ", query);
+        // do stuff
+    }
+    return (
+        <form onSubmit={handleSubmit} className="p-2 pl-5 border border-gray-300 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all w-full">
+            <input
+                type="text"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)} 
+                className="w-full p-2 focus:outline-none"
+            />
+            <button type="submit" className="hidden">Search</button>
+        </form>
     );
 }
 
