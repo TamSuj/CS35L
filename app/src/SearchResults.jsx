b695c763131d19b04cb10029c5428d2ff8a78b41
Megan Luu <megluu04@gmail.com>
2025-02-23 00:41:56 -0800

    Rename searchBar.jsx to uppercase

---
+++ app/src/SearchResults.jsx
@@ -0,0 +1,14 @@
+function SearchResults({ results }) {
+    return (
+      <div>
+        <h2>Search Results</h2>
+        <ul>
+          {results.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default SearchResults;
