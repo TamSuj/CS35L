3367ab37eca6afe71b46461566d0798b3ccedeec
Felicia Chen <feliciachen18@gmail.com>
2025-02-22 16:37:51 -0800

    working kind of omg

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,21 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import db from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/db.js";
 
-dotenv.config({path: '../../.env'}); // load in database and port
+dotenv.config({ path: "../../.env" }); // load in database and port
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('backend server is running');
+app.get("/", (req, res) => {
+  res.send("backend server is running");
 });
 
-app.get('/api', (req, res) => {
-    res.send('does this work?');
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+  console.log("hello");
 });
-  
+
+app.post("/api/register", (req, res) => {
+  res.send("POST Request Called");
+});
+
 app.listen(port, () => {
-    console.log(`Backend Server is running on http://localhost:${port}`);
+  console.log(`Backend Server is running on http://localhost:${port}`);
 });
