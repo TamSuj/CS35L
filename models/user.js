65b910b2f6047a599f99cd1df10ef653adb0e6fb
Gayvalin Sujaritchai <tammygayvalin@gmail.com>
2025-02-23 20:50:06 -0800

    Add statsSchema to store noteCount, followerCount, followingCount. Updated userScheme.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import statsSchema from "./stats.js";
+import tagSchema from "./tag.js";
 
 // Define the schema for the 'User' collection
 const userSchema = new mongoose.Schema(
@@ -8,7 +10,9 @@ const userSchema = new mongoose.Schema(
     name: { type: String, default: null },
     posts: { type: [String], default: [] }, //array of the posts created by this user
     bio: { type: String, default: null },
+    tags: { type: [tagSchema], default: [] }, //array of user's tag (subject, interest)
     profilePic: { type: String, default: null }, //URL of the pfp
+    stats: { type: statsSchema, default: () => ({}) }, //array of the user's stats (follower,  following, note count)
     password: { type: String, default: null }, // hashing function (?)
   },
   { collection: "user" }
