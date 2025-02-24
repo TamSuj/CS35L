89d560b9911f66c845211b18298948e53010f8a7
Megan Luu <megluu04@gmail.com>
2025-02-23 21:49:25 -0800

    Fix module issues

--- app/backend/routes/register.js
+++ app/backend/routes/register.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import userSchema from "../../../models/user.js";
+import userSchema from "../../models/user.js";
 
 var router = express.Router();
 
